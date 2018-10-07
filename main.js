3d0f03e09abaaa0abaa68ec444957c22e868c361
simenghe <simengschool@gmail.com>
2018-10-06 23:26:35 -0400

    ayy

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const electron = require('electron');
 const url = require('url');
 const path=require('path');
 
-const {app, BrowserWindow}=electron;
+const {app, BrowserWindow, Menu}=electron;
 
 let mainWindow;
+let addWindow;
 
 //Listen for app to be ready
 
@@ -17,4 +18,86 @@ app.on('ready',function(){
         protocol: 'file:',
         slashes :true
     }));
+    //Quit app when closed
+    mainWindow.on('closed',function(){
+        app.quit();
+    })
+    //Build menu from template
+    const mainMenu= Menu.buildFromTemplate(mainMenuTemplate);
+    //Insert menu
+    Menu.setApplicationMenu(mainMenu);
 });
+
+//handle add window
+function createAddWindow(){
+        //Create new window
+        addWindow=new BrowserWindow({
+            width:300,
+            height:200,
+            title: 'Get stock numbers'
+        });
+        
+
+        //load html into window
+        addWindow.loadURL(url.format({
+            pathname: path.join(__dirname,'addWindow.html'),
+            protocol: 'file:',
+            slashes :true
+        }));    
+        //garbage collection handle
+        addWindow.on('close',function(){
+            addWindow=null;
+        })
+
+}
+
+//Create menu template
+const mainMenuTemplate= [
+    {
+        label: 'File',
+        submenu:[
+            {
+                label: 'Add Item',
+                click(){
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items'
+            },
+            {
+                label: 'Quit',
+                accelerator:process.platform="darwin" ? 'Command+Q' :
+                'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+//If mac, add empty object to menu
+if(process.platform=='darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+// Add developer tools item if not in production
+if(process.env.NODE_ENV != 'production'){
+    mainMenuTemplate.push({
+        label:'Developer Tools',
+        submenu:[
+            {
+                label:'Toggle DevTools',
+                accelerator:process.platform="darwin" ? 'Command+I' :
+                'Ctrl+I',
+                click(item,focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
